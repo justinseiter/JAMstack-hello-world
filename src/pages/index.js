7515b749bb13fd210dc724ba4111a78053fefe95
Justin Seiter <justinseiter@gmail.com>
2018-08-29 12:51:50 -0500

    Initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hello freeCodeCamp!</h1>
+    <p>justin</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
